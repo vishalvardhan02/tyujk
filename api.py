c9d9aed11be6ec5979acd7f4630c86340ef69d3c
vishalvardhan02 <124067147+vishalvardhan02@users.noreply.github.com>
2023-02-06 21:40:31 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI
+import uvicorn
+from pydantic import BaseModel
+import pickle
+import numpy as np
+import pandas as pd
+import gunicorn
+import inspect
+
+df = pd.read_csv("Water_Quality.csv")
+
+params = ['ph', 'Hardness', 'Solids', 'Chloramines', 'Sulfate', 'Conductivity', 'Organic_carbon', 'Trihalomethanes', 'Turbidity']
+filled = []
+
+class model(BaseModel):
+    ph: str
+    Hardness: str
+    Solids: str
+    Chloramines: str
+    Sulfate: str
+    Conductivity: str
+    Organic_carbon: str
+    Trihalomethanes: str
+    Turbidity: str
+
+app = FastAPI()
+
+@app.get("/")
+def homeFunction():
+    return "Hello"
+
+@app.post("/Auto_fill")
+def autoFill(parameters: model):
+    random_fill = 1
+    attributes = inspect.getmembers(parameters, lambda a:not(inspect.isroutine(a)))
+    upper = {"ph":2, "Hardness":10, "Solids":10000, "Chloramines":2, "Sulfate":10, "Conductivity":10,"Organic_carbon":3, "Trihalomethanes":10, "Turbidity":2}
+    x = dict([a for a in attributes if not(a[0].startswith('__')) and not(a[0].startswith('_')) and not(a[0]=="Config")])
+    for i in params:
+        if(x[i]!=""):
+            filled.append(i)
+            random_fill = 0
+    if(random_fill==1):
+        random = list(df.sample(1).values)
+        return random
+    else:
+        final_df = df
+        final_df = final_df.drop("Potability", axis=1)
+        for i in filled:
+            value = round(float(x[i]))-1
+            if(value>10):
+                while(value%10!=0):
+                    value -=value%10
+            if(value>1000):
+                while(value%1000!=0):
+                    value -=value%1000
+            print(value)
+            final_df = final_df[(final_df[i]>=value) & (final_df[i]<value+upper[i])]
+        print(final_df)
+        random = list(final_df.values)
+        return random
+    
